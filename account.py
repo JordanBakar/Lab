23ee8628ca42fd51934f84ac6be0a27500dc3ed3
JordanBakar <113556116+JordanBakar@users.noreply.github.com>
2022-11-17 14:13:33 -0600

    Update account.py

--- account.py
+++ account.py
@@ -1,21 +1,21 @@
 class Account:
-    def __init__(self, name=0) -> None:
+    def __init__(self, name: str):
         self.__account_name = name
         self.__account_balance = 0
 
-    def deposit(self, amount):
+    def deposit(self, amount: float):
         if amount > 0:
             self.__account_balance += amount
             return True
         elif amount <= 0:
             return False
 
-    def withdraw(self, amount):
-        if amount > 0:
+    def withdraw(self, amount: float):
+        if amount <= 0 or amount > self.__account_balance:
+            return False
+        else:
             self.__account_balance -= amount
             return True
-        elif amount <= 0 or amount > self.__account_balance:
-            return False
 
     def get_balance(self):
         return self.__account_balance
